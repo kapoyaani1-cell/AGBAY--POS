e8a10b9c0b3cb0d7502169333493b86a4dc29c13
kapoyaani1-cell <kapoyaani1@gmail.com>
2026-05-06 00:56:24 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace project2
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
